1daa8d469e49808092223d54fc42854e0a7d8470
Alan <cortez.alan060@gmail.com>
2024-11-30 19:49:13 -0800

    continued work on reports page

--- app/reports/layout.tsx
+++ app/reports/layout.tsx
@@ -1,3 +1,4 @@
+import { IconButton } from "@/components/ui/icons";
 import { Heading } from "@/components/ui/typography";
 import Link from "next/link";
 import React from "react";
@@ -10,25 +11,34 @@ export default function ReportsLayout({
 }>) {
 
   return (
-    <div className="flex flex-col">
-        <div className="">
-          <Heading styles={{color: "text-gray-950"}}>
-            Reports
-          </Heading>
-          <div className="flex gap-4">
+    <main className="flex flex-col">
+        <div className="bg-emerald-900 h-32 p-6 flex w-full justify-between items-center">
+            <div className="flex flex-col gap-5">
+                <Heading styles={{weight: "font-semibold"}}>
+                    Reports
+                </Heading>
+                <div className="flex gap-5">
+                    <Link href="/">
+                        <Heading size="h3" styles={{weight: "font-medium"}}>
+                            At a Glance
+                        </Heading>
+                    </Link>
+                    <Link href="/">
+                        <Heading size="h3" styles={{weight: "font-medium", color: "text-gray-300 text-opacity-50"}}>
+                            History
+                        </Heading>
+                    </Link>
+                </div>
+            </div>
             <Link href="/">
-              <Heading size="h2" styles={{weight: "font-medium", color: "text-gray-950"}}>
-                At a Glance
-              </Heading>
+              <IconButton icon="CirclePlus" size="lg" align="right" iconColor="#030712">
+                <Heading size="h2" styles={{color: "text-gray-950", weight: "font-semibold"}}>
+                  Create
+                </Heading>
+              </IconButton>
             </Link>
-            <Link href="/">
-              <Heading size="h2" styles={{weight: "font-medium", color: "text-gray-950"}}>
-                History
-              </Heading>
-            </Link>
-          </div>
         </div>
         {children}
-    </div>
+    </main>
   );
 }
--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,22 +1,45 @@
 import React from "react";
 import { Heading } from "@/components/ui/typography"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { ImpactCard } from "@/components/ui/card";
+import { reportsPreview } from "@/data/previewData";
 
 export default function Reports() {
 
     return (
       <main className="grid grid-cols-2">
-      <section className="bg-sky-100 col-span-2 px-10 py-8 flex flex-col gap-4">
+      <section className="px-10 py-8 flex flex-col gap-4 w-full">
         <Heading styles={{color: "text-gray-950"}}>
           Your Impact
         </Heading>
-      </section>
-      <section className="px-10 py-8 flex flex-col gap-4">
+        <div className="flex items-center gap-4">
+          <Heading size='h2' styles={{color: "text-gray-800", weight: "font-medium"}}>
+            In the past 
+          </Heading>
+          <SelectTime />
+        </div>
+        <ImpactCard impactMetrics={reportsPreview} showDate={false}/>
         
-      </section>
-      <section className="px-10 py-8 flex flex-col gap-4">
-
       </section>
     </main>
     );
   }
   
+  function SelectTime() {
+    return (
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="7 days" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Week">7 Days</SelectItem>
+          <SelectItem value="Month">Month</SelectItem>
+          <SelectItem value="3 Months">3 Months</SelectItem>
+          <SelectItem value="6 Months">6 Months</SelectItem>
+          <SelectItem value="9 Months">9 Months</SelectItem>
+          <SelectItem value="Year">Year</SelectItem>
+        </SelectContent>
+      </Select>
+  
+    )
+  }
